8ec74d5c59f045c1cb56073ee3eb427804a9758a
T G SHIV KUMAR <115928544+shivtg0912@users.noreply.github.com>
2023-06-14 01:28:30 +0530

    Changes to css html and js

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
- let intro=document.querySelector('.intro');
- let logo=document.querySelector('.logo-header');
- let logoSpan=document.querySelectorAll('.logo');
- window.addEventListener('DOMContentLoaded',()=>{
-    setTimeout(()=>{
-        logoSpan.forEach((span,idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-                },(idx+2)*320)
-            });
-            setTimeout(()=>{
-                logoSpan.forEach((span,idx)=>{
-                    setTimeout(()=>{
-                        span.classList.remove('active');
-                        span.classList.add('fade');
-                    },(idx+2)*50);
-                })
-            },2000);
-            setTimeout(()=>{
-                intro.style.top='-100vh';
-            },2300);
-        })
-    })
+let intro=document.querySelector('.intro');
+let logo=document.querySelector('.logo-header');
+let logoSpan=document.querySelectorAll('.logo');
+window.addEventListener('DOMContentLoaded',()=>{
+   setTimeout(()=>{
+       logoSpan.forEach((span,idx)=>{
+           setTimeout(()=>{
+               span.classList.add('active');
+
+               },(idx+2)*320)
+           });
+           setTimeout(()=>{
+               logoSpan.forEach((span,idx)=>{
+                   setTimeout(()=>{
+                       span.classList.remove('active');
+                       span.classList.add('fade');
+                   },(idx+2)*50);
+               })
+           },2000);
+           setTimeout(()=>{
+               intro.style.top='-100vh';  
+           },2300);
+           setTimeout(()=>{document.body.style.overflowY="visible";
+       },2800);
+   })
+})
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+const hiddenELements=document.querySelectorAll('.hidden');
+hiddenELements.forEach((el)=>observer.observe(el));
+history.scrollRestoration = "manual";
+$(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+});
